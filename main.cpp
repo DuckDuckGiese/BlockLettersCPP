6721c187129fd4aa822413ceb3b4e5a65491ad1b
Ricky Giese <richard.giese@icloud.com>
2023-10-23 18:12:26 -0500

    completed

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "RRRR     GGG \n";
+    std::cout << "R   R   G   G\n";
+    std::cout << "R   R   G    \n";
+    std::cout << "RRRR    G GGG\n";
+    std::cout << "R  R    G   G\n";
+    std::cout << "R   R   G   G\n";
+    std::cout << "R   R    GGG \n";
+    return 0;
+}
